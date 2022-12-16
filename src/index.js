7a64515f6bfd02c116ece4a580386f5146be0c36
ankit711 <itsankitsingh711@edureka.co>
2022-12-16 22:26:25 +0530

    done today's work

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ let slide_img = document.querySelector(".slideImg img");
 slide_img.src =
   "https://9to5mac.com/wp-content/uploads/sites/6/2022/11/apple-tv-how-to-watch-echo-3.jpg?quality=82&strip=all&w=1280";
 let img_count = 0;
-async function getData() 
-{
-  let res = await fetch("https://63986336044fa481d69b935b.mockapi.io/movie");
+async function getData() {
+  let res = await fetch("https://63986336044fa481d69b935b.mockapi.io/film");
   let data = await res.json();
-  // console.log(data);
+  // cards(data);
   slide_show(data);
-  static_show(data);
-  if (img_count === data.length - 1) 
-  {
+  if (img_count === data.length - 1) {
     img_count = 0;
   }
   slide_img.src = data[img_count].poster;
@@ -18,36 +15,33 @@ async function getData()
 }
 setInterval(getData, 2000);
 
-// Sliding divs starts here 
+// Sliding divs starts here
 let slide = document.querySelector(".slide-track");
-let slide_static = document.querySelector(".static_slide-track");
 let slide_show = (data) => {
-    let arr = data.map((elem)=>{
-        return `
+  let arr = data.map((elem) => {
+    return `
             <div class="slide">
                 <img src=${elem.img2} alt=${elem.name}>
                 </img>
-                <div class="playbtn">
+                <div class="playbtn2">
                   <i class="fa-sharp fa-solid fa-circle-play"></i>
                 </div>
             </div>
-        `
-    })
-    slide.innerHTML = arr.join("");
+        `;
+  });
+  slide.innerHTML = arr.join("");
+};
+
+let main2 = document.querySelector(".main2 img");
+let img_count2=0;
+async function getData2() {
+  let res = await fetch("https://63986336044fa481d69b935b.mockapi.io/film");
+  let data = await res.json();
+  if (img_count2 === data.length - 1) {
+    img_count2 = 0;
+  }
+  main2.src = data[img_count2].poster;
+  img_count2++;
 }
 
-function static_show(data)
-{
-  let arr = data.map((elem)=>{
-    return `
-    <div class="slide">
-        <img src=${elem.img1} alt=${elem.name}>
-        </img>
-        <div class="playbtn">
-          <i class="fa-sharp fa-solid fa-circle-play"></i>
-        </div>
-    </div>
-`
-  })
-  slide_static.innerHTML = arr.join("")
-}
+setInterval(getData2, 2000);
